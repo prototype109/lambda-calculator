1e0c713a07e9bcb4b189b07c9418e7291a3c294a
Brandon Nichols <brandonaronnichols@gamil.com>
2019-07-26 02:15:20 -0500

    Added Numbers component with NumberButtons as well as styling for those buttons

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,35 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
+  let style = {
+    borderRadius: "100%",
+    background: "blue",
+    color: "white",
+    border: "none"
+    //width: "148px"
+  };
+
+  const checkIfZero = () =>{
+    console.log(props.number);
+    if(props.number !== "0"){
+      style.width = "88px";
+      style.height = "88px";
+      return style;
+    }
+    else{
+      style.width = "208px";
+      style.height = "88px";
+      style.borderRadius = "51%";
+      return style;
+    }
+  }
+
   return (
     <>
+        <button style={checkIfZero()}>{props.number}</button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,33 @@
-import React from "react";
-
+import React, {useState} from "react";
+import {numbers} from '../../../data';
+import NumberButton from './NumberButton';
 //import any components needed
 
 //Import your array data to from the provided data file
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [currentNumber, setCurrentNumber] = useState();
+  const style = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    width: "350px"
+  };
+  
   return (
-    <div>
+    <div style={style}>
+      {
+        numbers.map(num => {
+          //setCurrentNumber(num);
+          return <NumberButton number={num} />
+        })
+      }
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
     </div>
   );
 };
+
+export default Numbers;
